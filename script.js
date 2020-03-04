60f0ec906b9a21c75b6b3dfa0b2430d468ad6616
jlchen6 <lulei94@gmail.com>
2020-03-04 14:27:13 -0600

    Completed password generator function

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var generateBtn = document.querySelector("#generate");
 //Function to generate a password based on user requirements
 function generatePassword(){
   //Define variables
+  var password = "";
   var passLength;
-  var specialChars = []; 
+  var charTypes = ["lowercase", "uppercase", "numeric", "special"]; 
+  var charsList = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*`~-_=+',./?;:"];
+  var passListSpecial = [];
 
   //Ask for password length. Must be between 8 and 128 chars
   //Continue asking for a valid password length until user enters a valid number
@@ -22,10 +25,31 @@ function generatePassword(){
   }
 
   //Ask for character types to include, such as lowercase, uppercase, numeric, and/or special characters
+  //use Confirm form to figure out which special characters to include, and add those to the list of characters to use
+  while(passListSpecial.length < 1){
+    for (let i = 0; i < charTypes.length; i++) {
+      const charType = charTypes[i];
+      if(confirm("Would you like to require " + charType + " characters?")){
+        passListSpecial.push(charType);
+      }    
+    }
+  
+  }
 
   //Generate password
-  
 
+  //For each character in the password, pick a random character from the chosen character types
+  for (let i = 0; i < passLength; i++) {   
+  //First pick the type of character by choosing a random character type from the list
+    let type = passListSpecial[Math.floor(Math.random() * passListSpecial.length)];
+
+  //Then pick out a random character from that type.
+    let charOptions = charsList[charTypes.indexOf(type)];
+    password += charOptions.charAt(Math.floor(Math.random() * charOptions.length));
+  }
+
+  //Return password
+  return password;
 }
 
 // Write password to the #password input
